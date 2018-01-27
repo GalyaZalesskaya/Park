81088c413ffe087c80f66cf1c1db8db9d676a338
GalyaZalesskaya <33455576+GalyaZalesskaya@users.noreply.github.com>
2018-01-27 15:50:50 +0300

    Add files via upload

--- Grass.cpp
+++ Grass.cpp
@@ -1,59 +1,94 @@
 #include "Grass.h"
 
-void Grass::reproduction(Live* mat_g[], std::vector<Live*>* animals) 
+void Grass::reproduction(Live* mat_g[], std::list<Live*>* animals)
 {
 	short choice = rand() % 4 + 1;
-	short del_x = 0, del_y = 0;
-	switch (choice)
+	short del_x = 0, del_y = 0, att = 0;
+	int x = get_x(), y = get_y();
+
+	/*if ((y < COL) && (*(mat_g + (x - 1)*COL + y + 1) == nullptr))
 	{
-	case 1: //right
-		if ((get_y() < COL) && (*(mat_g + get_x()*ROW + get_y() + 1) == nullptr))
-		{
-			del_y = 1;
-		}
-	case 2://left
-		if ((get_y() > 1) && (*(mat_g + get_x()*ROW + get_y() - 1) == nullptr))
+		del_y = 1;
+	}
+	else 
+		if ((y > 1) && (*(mat_g + (x - 1)*COL + y - 1) == nullptr))
 		{
 			del_y = -1;
 		}
+		else
+			if ((x > 0) && (*(mat_g + (x - 1)*COL + y) == nullptr))
+			{
+				del_x = -1;
+			}
+			else
+				if ((x < ROW) && (*(mat_g + (x)*COL + y) == nullptr))
+				{
+				del_x = 1;
+	}*/
+
+
+	switch (choice)
+	{
+	case 1: //right
+		if ((y < COL) && (*(mat_g + (x-1)*COL + y + 1) == nullptr))
+		{
+			del_y = 1;
+			break;
+		}
+	case 2://left
+		if ((y > 1) && (*(mat_g + (x-1)*COL + y - 1) == nullptr))
+		{
+			del_y = -1;
+			break;
+		}
 	case 3://up
-		if ((get_x() > 1) && (*(mat_g + (get_x() - 1)*ROW + get_y()) == nullptr))
+		if ((x > 0) && (*(mat_g + (x - 1)*COL + y) == nullptr))
 		{
 			del_x = -1;
+			break;
 		}
 	case 4://down
-		if ((get_x() < ROW) && (*(mat_g + (get_x() + 1)*ROW + get_y()) == nullptr))
+		if ((x < ROW) && (*(mat_g + (x)*COL + y) == nullptr))
 		{
 			del_x = 1;
+			break;
 		}
 	}
+
 	if ((del_x!=0)||(del_y!=0)) 
 	{
-		Grass grass(get_x()+del_x, get_y()+del_y, 1);
-		animals->push_back(&grass);
-		*(mat_g + (get_x()+del_x)*ROW + (get_y()+del_y)) = &grass;
+		Grass* p_grass = new Grass(x + del_x, y + del_y, 1);
+		animals->push_back(p_grass);
+		*(mat_g + (x+del_x)*ROW + (y+del_y)) =p_grass;
 	}
 
 }
 
-void Grass::living(Live * mat_g[], std::vector<Live*>* animals)
+void Grass::living(Live * mat_g[], std::list<Live*>* animals)
 {
 	if (get_age() >= g_lifespan)
+	{
 		death(mat_g, animals);
-	else
-		if (get_age() >= rep_adulty)
-			reproduction(mat_g, animals);
+		return;
+	}
+	if (get_age() >= rep_adulty)
+		reproduction(mat_g, animals);
+	iteration_plus();
+
 }
 
 
-void Grass::death(Live* mat_g[], std::vector<Live*>* animals)
+void Grass::death(Live* mat_g[], std::list<Live*>* animals)
 {
 	*(mat_g+get_x()*ROW+get_y()) = nullptr;
-	auto itFound = std::find(animals->begin(), animals->end(), this);
-		if (itFound != animals->end())
-		{
-			animals->erase(itFound);
-		}
+	not_alive();
+	//animals -> remove(this);
+	//delete this;
+}
+
+void Grass::print()
+{
+	std::cout << get_x() << " " << get_y() << " " << get_age() << std::endl;
 }
 
 Grass::~Grass()
